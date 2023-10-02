e9ba97b8a40570f55c45c0dd514c8809f87027f8
Jacob Hawkins <jacob.hawkins010@gmail.com>
2023-10-02 02:05:29 -0400

    cryptmsg not animating

--- src/Pages/Home/Sections/projects/Projects.jsx
+++ src/Pages/Home/Sections/projects/Projects.jsx
@@ -2,22 +2,46 @@ import './projects.css';
 import Project from '../../Components/project/Project';
 import { proj } from '../../../../proj-list';
 import SpikeStat from './SpikeStat';
+import SpikeStatFrame from './SpikeStatFrame';
+import { useState } from 'react';
+import CryptMSGFrame from './CryptMSGFrame';
+import CryptMSG from './CryptMSG';
 
 export default function Projects() {
+    const [spikeShown, setSpikeShown] = useState(false);
+    const [cryptShown, setCryptShown] = useState(false);
+
     return (
         <div className='projects'>
             <div className='projects-wrapper'>
                 <h1 className='projects-title'>Projects</h1>
 
-                <div
-                    style={{
-                        display: 'block',
-                        width: '700px',
-                        height: '150px',
-                        backgroundColor: 'red',
-                    }}></div>
+                {!spikeShown && (
+                    <div onMouseEnter={() => setSpikeShown(true)}>
+                        <SpikeStatFrame />
+                    </div>
+                )}
+                {spikeShown && (
+                    <div onMouseLeave={() => setSpikeShown(false)} className='spikestathover'>
+                        <SpikeStat />
+                    </div>
+                )}
 
-                <SpikeStat />
+                {/* {!cryptShown && (
+                    <div onMouseEnter={() => setCryptShown(true)}>
+                        <CryptMSGFrame />
+                    </div>
+                )}
+                {cryptShown && (
+                    <div onMouseLeave={() => setCryptShown(false)} className='cryptmsghover'>
+                        <CryptMSG />
+                    </div>
+                )} */}
+
+                <div className='cryptmsg'>
+                    <CryptMSG />
+                    {/* <CryptMSGFrame /> */}
+                </div>
 
                 <div className='projects-list'>
                     {proj.map((p) => (
