5f0ffd4649c8809d43808115100e369a36ca51f0
Jacob Hawkins <jacob.hawkins010@gmail.com>
2023-10-01 23:43:14 -0400

    spikestat animation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import ScrollToTop from './ScrollToTop';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './Pages/Home/Index';
 // import About from './Pages/About/Index';
 import Mobile from './Pages/Mobile/Index';
 
 function App() {
-	if (window.innerWidth <= 640) {
-		return (
-			<Mobile />
-		);
-	} else {
-		return (
-			< Router >
-				<ScrollToTop />
-				<Routes>
-					<Route path="/" element={<Home />} />
-				</Routes>
-			</Router >
-		);
-	}
+    if (window.innerWidth <= 640) {
+        return <Mobile />;
+    } else {
+        return (
+            <Router>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                </Routes>
+            </Router>
+        );
+    }
 }
 
 export default App;
--- src/Pages/Home/Sections/projects/Projects.jsx
+++ src/Pages/Home/Sections/projects/Projects.jsx
@@ -1,20 +1,30 @@
-import "./projects.css"
-import Project from '../../Components/project/Project'
-import { proj } from "../../../../proj-list"
+import './projects.css';
+import Project from '../../Components/project/Project';
+import { proj } from '../../../../proj-list';
+import SpikeStat from './SpikeStat';
 
 export default function Projects() {
-  return (
-    <div className='projects'>
-        <div className="projects-wrapper">
-            <h1 className="projects-title">Projects</h1>
-            <hr className="projects-hr" />
+    return (
+        <div className='projects'>
+            <div className='projects-wrapper'>
+                <h1 className='projects-title'>Projects</h1>
 
-            <div className="projects-list">
-                {proj.map((p) => (
-                    <Project key={p.id} proj={p} />
-                ))}        
+                <div
+                    style={{
+                        display: 'block',
+                        width: '700px',
+                        height: '150px',
+                        backgroundColor: 'red',
+                    }}></div>
+
+                <SpikeStat />
+
+                <div className='projects-list'>
+                    {proj.map((p) => (
+                        <Project key={p.id} proj={p} />
+                    ))}
+                </div>
             </div>
         </div>
-    </div>
-  )
+    );
 }
