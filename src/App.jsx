da09f063e952ca76f3d9a85be8d63aeeb01dfa58
Jacob Hawkins <jacob.hawkins010@gmail.com>
2024-06-10 19:30:09 -0400

    restructure sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Navbar from './components/Navbar/Index';
+
 function App() {
     return (
         <div className='App'>
+            <Navbar />
             <h1>Text</h1>
         </div>
     );
